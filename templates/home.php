5d49309417a2d05bfd0693712e944de7c7fb4902
MARCHAL Simon <marchal.simon@gmail.com>
2020-12-06 15:15:43 +0100

    Get the debug output and display it in a <pre> tag

--- templates/home.php
+++ templates/home.php
@@ -1,10 +1,15 @@
 <?php
+$before = ob_get_contents();
+ob_clean();
 $output = file_get_contents(__DIR__ . '/_main.html');
 
 $title = 'GBAF';
 $output = preg_replace('/({TITLE})/', $title, $output);
 
 $body = '<h1>Accueil</h1>';
+if (!empty($before)) {
+    $body .= '<pre class="debug">' . $before . '</pre>';
+}
 $output = preg_replace('/({BODY})/', $body, $output);
 
-echo $output;
+print_r($output);
